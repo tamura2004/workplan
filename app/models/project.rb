1845f92722211dd37f8fd9bfae3f1f259d83d1cf
tamura <tamura2004@gmail.com>
2017-03-15 18:10:39 +0900

    Add Project::Order

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   belongs_to :month, required: true
   validates :number, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
+
+  def fullname
+    "#{number}:#{name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,29 @@ Rails.application.routes.draw do
   scope :api do
     get "works/chart"
 
-    resources :orders
+    resources :depts
+    resources :ranks
+
+    resources :projects do
+      scope module: :projects do
+        resources :orders
+      end
+    end
+
+    resources :groups
+    resources :systems
+
     resources :months
+    resources :users
+    resources :orders
+
     resources :works
     resources :assigns
     resources :costs
-    resources :projects
-    resources :systems
-    resources :users
-    resources :groups
-    resources :ranks
-    resources :depts
   end
 
-  root "users#index"
+  root "project#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+__END__
